96a7992b554e4a6ce29fd8cc9054df7793dfa5f4
Emi <edinh02@calpoly.edu>
2024-05-10 21:55:41 -0700

    button works

---
+++ Assets/Scripts/changeScene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void playIntro(){
+        SceneManager.LoadScene(1); 
+    }
+
+    public void playLvl1(){
+        SceneManager.LoadScene(2); 
+    }
+
+}
